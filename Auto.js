3d2ebeca05b7ae5ad947796df66f07f71db9d42c
Kaluz27 <eluna3@ucol.mx>
2019-02-08 15:31:53 -0600

    Metodos de lectura y escritura

--- Auto.js
+++ Auto.js
@@ -1,10 +1,44 @@
-class Auto{
-constructor(marca, modelo, color, kilometraje){
-    this.marca=marca;
-    this.modelo=modelo;
-    this.color=color;
-    this.kilometraje=kilometraje;
-    this.estado="Apagado";
-    
+export default class Auto {
+constructor(marca, modelo, color= "Rojo", kilometraje=0){
+    this._marca=marca;
+    this._modelo=modelo;
+    this._color=color;
+    this._kilometraje=kilometraje;
+    this._estado="Apagado";
 }
+
+get color(){ //Get es un metodo de lectura
+    return this._color;
 }
+set color(color){ //Set es un metodo de escritura
+    this._color= color.toUpperCase();
+}
+
+
+mostrarEstado(){
+    console.log(`El auto ${this._marca}  del año ${this._modelo} tiene un color ${this._color} tiene un kilometraje de ${this._kilometraje}  y se encuentra ${this._estado}` );
+}
+
+encender(){
+    this._estado = "Encendido"
+    this.mostrarEstado()
+}
+apagar(){
+    this._estado = "Apagado"
+    this.mostrarEstado()
+}
+
+avanazar(velocidad, tiempo){
+    if(this._estado === "Apagado"){
+        console.log('El auto no avanza porque está apagado')
+    }
+    else{
+        let distancia= velocidad * tiempo;
+        this._kilometraje = this._kilometraje + distancia;
+       this.mostrarEstado();
+      
+    }
+}
+
+}
+
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-console.log("Hola,mundo!");
+import Auto from "./Auto.js";
+
+let auto1= new Auto("Ford", 2000, "rojo", 100)
+let auto2= new Auto("Nissan", 2018, "Plata", 50)
+let auto3 = new Auto("BMW", 2000)
+
+console.log(`Color ${auto1.color}`);//Lectura
+console.log(`Color ${auto2.color}`);//Lectura
+
+auto1.color = "Amarillo"//Escritura
+auto2.color = "Azul"//Escritura
+
+
+auto2.encender()
+auto3.encender()
+
+auto1.encender();
+auto1.avanazar(50, 2);
+auto1.apagar();
+auto1.avanazar(20, 1)
